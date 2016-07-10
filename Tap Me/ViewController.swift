2e303096d854fd9c8488eb7e0749a5262e8b9f0b
Joseph Leiferman <joseph.leiferman@gmail.com>
2016-07-10 14:43:35 -0500

    iOS app
    An iOS game with the main goal of seeing how many taps you can get in a
    certain time frame.

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -7,11 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var timerLabel: UILabel!
+    var buttonBeep : AVAudioPlayer?
+    var secondBeep : AVAudioPlayer?
+    var backgroundMusic : AVAudioPlayer?
+    var taps = 0
+    var seconds = 0
+    var timer = NSTimer()
+    
+    @IBAction func buttonPressed() {
+        taps++
+        scoreLabel.text = "Score: \(taps)"
+        buttonBeep?.play()
+    }
+    func setupAudioPlayerWithFile(file:NSString, type:NSString) -> AVAudioPlayer? {
+        //1
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        
+        let url = NSURL.fileURLWithPath(path!)
+        
+        //2
+        var audioPlayer: AVAudioPlayer?
+        
+        //3
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print("Player not avaiable")
+        }
+        return audioPlayer
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let buttonBeep = self.setupAudioPlayerWithFile("ButtonTap", type: "wav") {
+            self.buttonBeep = buttonBeep
+        }
+        if let secondBeep = self.setupAudioPlayerWithFile("SecondBeep", type: "wav") {
+            self.secondBeep = secondBeep
+        }
+        if let backgroundMusic = self.setupAudioPlayerWithFile("HallOfTheMountainKing", type: "mp3") {
+            self.backgroundMusic = backgroundMusic
+        }
+        // sets backgroundColor to bg_tile.png
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_tile.png")!)
+        // sets score label to field_score.png
+        scoreLabel.backgroundColor = UIColor(patternImage: UIImage(named: "field_score.png")!)
+        // sets score label to field_time.png
+        timerLabel.backgroundColor = UIColor(patternImage: UIImage(named: "field_time.png")!)
+        setupGame()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +69,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupGame() {
+        seconds = 30
+        taps = 0
+        backgroundMusic?.volume = 0.3
+        backgroundMusic?.play()
+        
+        timerLabel.text = "Time: \(seconds)"
+        scoreLabel.text = "Score: \(taps)"
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"),
+            userInfo: nil, repeats: true)
+        
+    }
+    
+    func subtractTime() {
+        seconds--
+        timerLabel.text = "Time: \(seconds)"
+        secondBeep?.play()
+        
+        
+        if( seconds == 0) {
+            timer.invalidate()
+            let alert = UIAlertController(title: "Time is up!", message: "You scored \(taps) points", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title:"Play Again", style: UIAlertActionStyle.Default, handler: {
+                action in self.setupGame()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }
+    }
 }
 
